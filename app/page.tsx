80f6ccf40c5e6a65c56686b6ba7329b4a1a319d3
Nyore <aamonthedon@gmail.com>
2023-06-30 14:26:11 +0100

    create car details modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components";
+import CarCatalogue from "@/components/CarCatalogue";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <CarCatalogue />
     </main>
   );
 }
